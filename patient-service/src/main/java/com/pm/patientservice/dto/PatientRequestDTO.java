832721849bc9e86a76b446f74f19f40c383569c8
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2025-12-04 13:35:45 +0100

    feat: complete patient CRUD with validation and REST endpoints

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,67 @@
+package com.pm.patientservice.dto;
+
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registred date is required")
+    private String registredDate;
+
+
+    public @NotBlank(message = "Name is required") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is required") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank(message = "Email is required") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is required") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Address is required") String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotBlank(message = "Address is required") String address) {
+        this.address = address;
+    }
+
+    public @NotBlank(message = "Date of birth is required") String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotBlank(message = "Date of birth is required") String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRegistredDate() {
+        return registredDate;
+    }
+
+    public void setRegistredDate(String registredDate) {
+        this.registredDate = registredDate;
+    }
+}
