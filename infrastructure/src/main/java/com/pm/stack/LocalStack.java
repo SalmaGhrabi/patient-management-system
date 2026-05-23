4cf76fb3d9b7acc6d4d02ac8ae4928e0d4c8b485
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-05-23 22:55:14 +0100

    feat(infrastructure): create msk cluster

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,12 +1,13 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
-import software.amazon.awscdk.services.ec2.InstanceClass;
-import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
+
+import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -26,6 +27,8 @@ public class LocalStack extends Stack {
 
         CfnHealthCheck patientDbHealthCheck =
                 createDbHealthCheck(patientServiceDb,"PatientServiceDBHealthCheck");
+
+        CfnCluster mskCluster = createMskCluster();
     }
 
     private Vpc createVpc() {
@@ -61,6 +64,22 @@ public class LocalStack extends Stack {
                         .failureThreshold(3)
                         .build())
                 .build();
+    }
+
+    private CfnCluster createMskCluster () {
+        return  CfnCluster.Builder.create(this, "MskCluster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                        .clientSubnets(vpc.getPrivateSubnets().stream()
+                                .map(ISubnet::getSubnetId)
+                                .collect(Collectors.toList()))
+                        .brokerAzDistribution("DEFAULT")
+                        .build())
+                .build();
+
     }
 
     public static void main(final String[] args) {
